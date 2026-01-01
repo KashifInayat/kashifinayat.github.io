6e8eab988d7be348da3a793c34e9ad7b44122e27
KashifInayat <kinayat@bsc.es>
2026-01-01 02:18:27 +0000

    jobs: add PostDoc Position - Automatic Exploration of RISC-V Compressed Instruction Set Extensions | Sr. Physical Design Engineer (CPU cores) | Physical Design Engineer, Staff / Sr. Staff | Software Security Engineer (Multiple Levels Available)

--- js/jobs-data.js
+++ js/jobs-data.js
@@ -12,20 +12,22 @@ var jobsData = {
           "locations": [],
           "files": [
             {
-              "title": "Physical Design Engineer, Staff / Sr. Staff",
-              "company": "Qualcomm",
-              "city": "Cork, Ireland",
-              "rsu": "Possible",
-              "salary": "Yes",
-              "apply": "https://www.linkedin.com/jobs/view/4323382587/?refId=I9NW9FRlvNT4PZt%2BvXS2hQ%3D%3D&trackingId=I9NW9FRlvNT4PZt%2BvXS2hQ%3D%3D"
-            },
-            {
-              "title": "Sr. Physical Design Engineer (CPU cores), Cork, Ireland",
+              "title": "Sr. Physical Design Engineer (CPU cores)",
               "company": "AMD",
-              "city": "Cork, Ireland",
+              "city": "Sr. Physical Design Engineer (CPU cores)",
               "rsu": "",
               "salary": "",
-              "apply": "https://www.linkedin.com/jobs/view/4264442415/?alternateChannel=search&refId=NotAvailable&trackingId=99qijnq%2BS5acSjE6dsX%2Bbg%3D%3D&trk=d_flagship3_search_srp_jobs&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_jobs%3BI%2FOsEsVnRQKgSTUsXg1gpQ%3D%3D"
+              "apply": "https://www.linkedin.com/jobs/view/4264442415/?alternateChannel=search&refId=NotAvailable&trackingId=99qijnq%2BS5acSjE6dsX%2Bbg%3D%3D&trk=d_flagship3_search_srp_jobs&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_jobs%3BI%2FOsEsVnRQKgSTUsXg1gpQ%3D%3D",
+              "slug": "sr-physical-design-engineer-cpu-cores-amd"
+            },
+            {
+              "title": "Physical Design Engineer, Staff / Sr. Staff",
+              "company": "Qualcomm",
+              "city": "Physical Design Engineer, Staff / Sr. Staff",
+              "rsu": "",
+              "salary": "",
+              "apply": "https://www.linkedin.com/jobs/view/4323382587/?refId=I9NW9FRlvNT4PZt%2BvXS2hQ%3D%3D&trackingId=I9NW9FRlvNT4PZt%2BvXS2hQ%3D%3D",
+              "slug": "physical-design-engineer-staff-sr-staff-qualcomm"
             }
           ]
         },
@@ -52,10 +54,11 @@ var jobsData = {
             {
               "title": "Software Security Engineer (Multiple Levels Available)",
               "company": "Qualcomm",
-              "city": "Cork, Ireland",
-              "rsu": "Possible",
-              "salary": "Yes",
-              "apply": "https://www.linkedin.com/jobs/view/4340602188/?refId=rm3ppQOC7wbLIsR%2FE5bewA%3D%3D&trackingId=rm3ppQOC7wbLIsR%2FE5bewA%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_content_view%3B7UAtK0YdQBmmZo0AbEHE0w%3D%3D&lici=rm3ppQOC7wbLIsR%2FE5bewA%3D%3D"
+              "city": "Software Security Engineer (Multiple Levels Available)",
+              "rsu": "",
+              "salary": "",
+              "apply": "https://www.linkedin.com/jobs/view/4340602188/?refId=rm3ppQOC7wbLIsR%2FE5bewA%3D%3D&trackingId=rm3ppQOC7wbLIsR%2FE5bewA%3D%3D&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_content_view%3B7UAtK0YdQBmmZo0AbEHE0w%3D%3D&lici=rm3ppQOC7wbLIsR%2FE5bewA%3D%3D",
+              "slug": "software-security-engineer-multiple-levels-available-qualcomm"
             }
           ]
         },
@@ -146,9 +149,11 @@ var jobsData = {
             {
               "title": "PostDoc Position - Automatic Exploration of RISC-V Compressed Instruction Set Extensions",
               "company": "Munich University",
-              "city": "Munich, Germany",
-              "salary": "Yes",
-              "apply": "https://wallento.cs.hm.edu/job-postdoc-scale4edge/"
+              "city": "Automatic Exploration of RISC-V Compressed Instruction Set Extensions",
+              "rsu": "",
+              "salary": "",
+              "apply": "https://wallento.cs.hm.edu/job-postdoc-scale4edge/",
+              "slug": "postdoc-position-automatic-exploration-of-risc-v-compressed-instruction-set-extensions-munich-university"
             }
           ]
         },
